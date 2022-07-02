a5af482dee0ac0439b894246953b218a51038846
Swikar Ramdam <swikarramdam@gmail.com>
2022-07-02 20:53:50 +0545

    variable

---
+++ variable.php
@@ -0,0 +1,11 @@
+<?php
+    // echo "Swikar";
+    // <!-- variables start with $  -->
+    $name = "Swikar";
+    $income = 200.8;
+    $nature = "introvert";
+    $Nature = "dark, breeezy and peaceful";
+    echo "I am $name and I earn $income per minute. <br> ";
+    echo "He is a bit of $nature type.<br>";
+    echo "I love nature when it is $Nature.";
+?>
